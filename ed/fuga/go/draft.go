d025b0defa1cd41fe3b3856a86f4dc5a7d837020
Guilherme Marques <mr.gmrsouza@gmail.com>
2026-03-16 14:43:06 -0300

    feat(helicopter): helicopter activity resolved

--- ed/fuga/go/draft.go
+++ ed/fuga/go/draft.go
@@ -1,5 +1,33 @@
 package main
+
 import "fmt"
+
+func fuga(H int, P int, F int, D int) string {
+	pos := F
+
+	for {
+		if D == -1 {
+			pos = (pos - 1 + 16) % 16
+		} else {
+			pos = (pos + 1) % 16
+		}
+
+		if pos == P {
+			return "N"
+		}
+		if pos == H {
+			return "S"
+		}
+	}
+}
 func main() {
-    fmt.Println("qxcode")
+	var H int
+	var P int
+	var F int
+	var D int
+
+	fmt.Scan(&H, &P, &F, &D)
+
+	fmt.Println(fuga(H, P, F, D))
+
 }
