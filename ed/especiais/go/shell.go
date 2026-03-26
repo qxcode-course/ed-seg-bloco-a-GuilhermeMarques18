bac5963d27e76d1de8dfc5eff017a0d374d06cf7
Guilherme Marques <mr.gmrsouza@gmail.com>
2026-03-26 01:04:10 -0300

    feat(especiais): activity in progress

---
+++ ed/especiais/go/shell.go
@@ -0,0 +1,164 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"sort"
+	"strconv"
+	"strings"
+)
+
+type Pair struct {
+	One int
+	Two int
+}
+
+func occurr(vet []int) []Pair {
+	list := make(map[int]int)
+	slice := []int{}
+	for _, v := range vet {
+		absV := v
+		if v < 0 {
+			absV = -v
+		}
+		list[absV]++
+	}
+
+	for k := range list {
+		slice = append(slice, k)
+	}
+	sort.Ints(slice)
+	result := []Pair{}
+
+	for _, v := range slice {
+		result = append(result, Pair{v, slice[v]})
+	}
+	return result
+}
+
+func teams(vet []int) []Pair {
+	if len(vet) == 0 {
+		return []Pair{}
+	}
+	slice := []Pair{}
+	count := 1
+
+	for i := 0; i < len(vet)-1; i++ {
+		if i == len(vet)-1 || vet[i] != vet[i+1] {
+			slice = append(slice, Pair{vet[i], count})
+			count = 1
+		} else {
+			count++
+		}
+	}
+	return slice
+}
+
+func mnext(vet []int) []int {
+	_ = vet
+	return nil
+}
+
+func alone(vet []int) []int {
+	_ = vet
+	return nil
+}
+
+func couple(vet []int) int {
+	_ = vet
+	return 0
+}
+
+func hasSubseq(vet []int, seq []int, pos int) bool {
+	_ = vet
+	_ = seq
+	_ = pos
+	return false
+}
+
+func subseq(vet []int, seq []int) int {
+	_ = vet
+	_ = seq
+	return -1
+}
+
+func erase(vet []int, posList []int) []int {
+	_ = vet
+	_ = posList
+	return nil
+}
+
+func clear(vet []int, value int) []int {
+	_ = vet
+	_ = value
+	return nil
+}
+
+func main() {
+	scanner := bufio.NewScanner(os.Stdin)
+	for {
+		fmt.Print("$")
+		if !scanner.Scan() {
+			break
+		}
+		line := scanner.Text()
+		args := strings.Split(line, " ")
+		fmt.Println(line)
+
+		switch args[0] {
+		case "occurr":
+			printSlice(occurr(str2vet(args[1])))
+		case "teams":
+			printSlice(teams(str2vet(args[1])))
+		case "mnext":
+			printSlice(mnext(str2vet(args[1])))
+		case "alone":
+			printSlice(alone(str2vet(args[1])))
+		case "erase":
+			printSlice(erase(str2vet(args[1]), str2vet(args[2])))
+		case "clear":
+			val, _ := strconv.Atoi(args[2])
+			printSlice(clear(str2vet(args[1]), val))
+		case "subseq":
+			fmt.Println(subseq(str2vet(args[1]), str2vet(args[2])))
+		case "couple":
+			fmt.Println(couple(str2vet(args[1])))
+		case "end":
+			return
+		default:
+			fmt.Println("Invalid command")
+		}
+	}
+}
+
+// Funções auxiliares
+
+func str2vet(str string) []int {
+	if str == "[]" {
+		return nil
+	}
+	str = str[1 : len(str)-1]
+	parts := strings.Split(str, ",")
+	var vet []int
+	for _, part := range parts {
+		num, _ := strconv.Atoi(strings.TrimSpace(part))
+		vet = append(vet, num)
+	}
+	return vet
+}
+
+func printSlice[T any](vet []T) {
+	fmt.Print("[")
+	for i, x := range vet {
+		if i > 0 {
+			fmt.Print(", ")
+		}
+		fmt.Print(x)
+	}
+	fmt.Println("]")
+}
+
+func (p Pair) String() string {
+	return fmt.Sprintf("(%v, %v)", p.One, p.Two)
+}
