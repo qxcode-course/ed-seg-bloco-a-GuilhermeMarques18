579b088748905279806075ca377ce5f5f88bdc1c
Guilherme Marques <mr.gmrsouza@gmail.com>
2026-04-22 00:24:16 -0300

    feat(resto): activity completed

--- ed/resto/src/go/draft.go
+++ ed/resto/src/go/draft.go
@@ -1,5 +1,19 @@
 package main
+
 import "fmt"
+
+func div(n int) {
+	if n == 0 {
+		return
+	}
+	div(n / 2)
+
+	resto := n % 2
+	result := n / 2
+	fmt.Println(result, resto)
+}
 func main() {
-    fmt.Println("Hello, World!")
+	var n int
+	fmt.Scan(&n)
+	div(n)
 }
