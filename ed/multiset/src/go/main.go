dd44bd76f71be937699838a0664d8dab65f3e950
Guilherme Marques <mr.gmrsouza@gmail.com>
2026-05-12 18:29:10 -0300

    feat(multiset): activity completed

--- ed/multiset/src/go/main.go
+++ ed/multiset/src/go/main.go
@@ -4,8 +4,103 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"sort"
+	"strconv"
 	"strings"
 )
+
+type MultiSet struct {
+	data     []int
+	size     int
+	capacity int
+}
+
+func NewMultiSet(capacity int) *MultiSet {
+	return &MultiSet{data: make([]int, capacity), size: 0, capacity: capacity}
+}
+
+func (ms *MultiSet) Insert(value int) {
+	if ms.size == ms.capacity {
+		newCap := ms.capacity * 2
+
+		if newCap == 0 {
+			newCap = 1
+		}
+		newData := make([]int, newCap)
+		copy(newData, ms.data[:ms.size])
+		ms.data = newData
+		ms.capacity = newCap
+	}
+
+	idx := sort.SearchInts(ms.data[:ms.size], value)
+
+	for i := ms.size; i > idx; i-- {
+		ms.data[i] = ms.data[i-1]
+	}
+
+	ms.data[idx] = value
+	ms.size++
+}
+
+func (ms *MultiSet) String() string {
+	if ms.size == 0 {
+		return "[]"
+	}
+	var strValues []string
+	for i := 0; i < ms.size; i++ {
+		strValues = append(strValues, strconv.Itoa(ms.data[i]))
+	}
+	return "[" + strings.Join(strValues, ", ") + "]"
+}
+
+func (ms *MultiSet) Contains(value int) bool {
+
+	idx := sort.SearchInts(ms.data[:ms.size], value)
+
+	return idx < ms.size && ms.data[idx] == value
+}
+
+func (ms *MultiSet) Erase(value int) {
+	idx := sort.SearchInts(ms.data[:ms.size], value)
+	if idx < ms.size && ms.data[idx] == value {
+
+		for i := idx; i < ms.size-1; i++ {
+			ms.data[i] = ms.data[i+1]
+		}
+
+		ms.size--
+	} else {
+		fmt.Println("value not found")
+	}
+}
+
+func (ms *MultiSet) Count(value int) int {
+	idx := sort.SearchInts(ms.data[:ms.size], value)
+	count := 0
+
+	for i := idx; i < ms.size && ms.data[i] == value; i++ {
+		count++
+	}
+
+	return count
+}
+
+func (ms *MultiSet) Unique() int {
+	if ms.size == 0 {
+		return 0
+	}
+	unique := 1
+	for i := 1; i < ms.size; i++ {
+		if ms.data[i] != ms.data[i-1] {
+			unique++
+		}
+	}
+	return unique
+}
+
+func (ms *MultiSet) Clear() {
+	ms.size = 0
+}
 
 func Join(slice []int, sep string) string {
 	if len(slice) == 0 {
@@ -21,7 +116,7 @@ func Join(slice []int, sep string) string {
 func main() {
 	var line, cmd string
 	scanner := bufio.NewScanner(os.Stdin)
-	// ms := NewMultiSet(0)
+	var ms *MultiSet
 
 	for scanner.Scan() {
 		fmt.Print("$")
@@ -37,21 +132,32 @@ func main() {
 		case "end":
 			return
 		case "init":
-			// value, _ := strconv.Atoi(args[1])
-			// ms = NewMultiSet(value)
+			value, _ := strconv.Atoi(args[1])
+			ms = NewMultiSet(value)
 		case "insert":
-			// for _, part := range args[1:] {
-			// 	value, _ := strconv.Atoi(part)
-			// }
+			for _, part := range args[1:] {
+				value, _ := strconv.Atoi(part)
+				ms.Insert(value)
+			}
 		case "show":
+			fmt.Println(ms.String())
 		case "erase":
-			// value, _ := strconv.Atoi(args[1])
+			val, _ := strconv.Atoi(args[1])
+			ms.Erase(val)
 		case "contains":
-			// value, _ := strconv.Atoi(args[1])
+			val, _ := strconv.Atoi(args[1])
+			if ms.Contains(val) {
+				fmt.Println("true")
+			} else {
+				fmt.Println("false")
+			}
 		case "count":
-			// value, _ := strconv.Atoi(args[1])
+			val, _ := strconv.Atoi(args[1])
+			fmt.Println(ms.Count(val))
 		case "unique":
+			fmt.Println(ms.Unique())
 		case "clear":
+			ms.Clear()
 		default:
 			fmt.Println("fail: comando invalido")
 		}
