02f69491de058554af990fbeee27fb585c8571de
Guilherme Marques <mr.gmrsouza@gmail.com>
2026-06-01 17:04:03 -0300

    feat(eval1-queimada): activity completed

--- eval1/queimada/src/go/main.go
+++ eval1/queimada/src/go/main.go
@@ -7,11 +7,20 @@ import (
 )
 
 func burnTrees(grid [][]rune, l, c int) {
-	_, _, _ = grid, l, c
-	// se estiver fora da matriz, retorne
-	// se o elemento atual não for uma arvore, retorne
-	// queime a arvore colocando o caractere 'o' na posição atual
-	// chame a recursão para todos os 4 vizinhos
+	if l < 0 || c < 0 || l > len(grid)-1 || c > len(grid[0])-1 {
+		return
+	}
+
+	if grid[l][c] != '#' {
+		return
+	}
+
+	grid[l][c] = 'o'
+
+	burnTrees(grid, l-1, c)
+	burnTrees(grid, l+1, c)
+	burnTrees(grid, l, c-1)
+	burnTrees(grid, l, c+1)
 }
 
 func main() {
